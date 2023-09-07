ec330752cbe563a5a62250fc3f02b46db007eade
Achilles-Dev <solohag92@gmail.com>
2023-09-07 10:45:05 +0000

    Add Header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
+import Header from '@/components/Header'
 import '@/styles/globals.css'
+import { NextUIProvider } from '@nextui-org/react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <>
+        <Header />
+        <Component {...pageProps} />
+      </>
+    </NextUIProvider>
+  )
 }
